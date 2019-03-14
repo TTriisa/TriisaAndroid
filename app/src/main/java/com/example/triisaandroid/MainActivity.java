a7185bc3d63995d13f2dd49b19d527cccc775870
Teet Triisa <ttriisa@tlu.ee>
2019-03-14 21:15:56 +0200

    Adding Fragment tab view and Countdown timer functionality

--- app/src/main/java/com/example/triisaandroid/MainActivity.java
+++ app/src/main/java/com/example/triisaandroid/MainActivity.java
@@ -1,166 +1,72 @@
 package com.example.triisaandroid;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.view.MenuItem;
-import android.widget.TextView;
-import android.os.Handler;
-import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
-
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView ;
-
-    Button start, pause, reset, lap ;
-
-    long MillisecondTime, StartTime, TimeBuff, TimeLaps, UpdateTime = 0L ;
-
-    Handler handler;
-
-    int Seconds, Minutes, MilliSeconds;
-
-    int LapNumber = 1;
-
-    ListView listView ;
-
-    String[] ListElements = new String[] {  };
-
-    List<String> ListElementsArrayList ;
-
-    ArrayAdapter<String> adapter ;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView)findViewById(R.id.textView);
-        start = (Button)findViewById(R.id.button);
-        pause = (Button)findViewById(R.id.button2);
-        reset = (Button)findViewById(R.id.button3);
-        lap = (Button)findViewById(R.id.button4) ;
-        listView = (ListView)findViewById(R.id.listview1);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        handler = new Handler() ;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        ListElementsArrayList = new ArrayList<String>(Arrays.asList(ListElements));
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
 
-        adapter = new ArrayAdapter<String>(MainActivity.this,
-                android.R.layout.simple_list_item_1,
-                ListElementsArrayList
-        );
-
-        listView.setAdapter(adapter);
-
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                StartTime = SystemClock.uptimeMillis();
-                handler.postDelayed(runnable, 0);
-
-                reset.setEnabled(false);
-                pause.setEnabled(true);
-                start.setEnabled(false);
-            }
-        });
-
-        pause.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                TimeBuff += MillisecondTime;
-
-                handler.removeCallbacks(runnable);
-
-                reset.setEnabled(true);
-                pause.setEnabled(false);
-                start.setEnabled(true);
-            }
-        });
-
-        reset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                MillisecondTime = 0L ;
-                StartTime = 0L ;
-                TimeBuff = 0L ;
-                TimeLaps = 0L ;
-                UpdateTime = 0L ;
-                Seconds = 0 ;
-                Minutes = 0 ;
-                MilliSeconds = 0 ;
-                LapNumber = 1;
-
-                textView.setText("00:00:00");
-
-                ListElementsArrayList.clear();
-
-                adapter.notifyDataSetChanged();
-            }
-        });
-
-        lap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                UpdateTime = UpdateTime - TimeLaps;
-
-                Seconds = (int) ((UpdateTime) / 1000) ;
-
-                Minutes = Seconds / 60;
-
-                Seconds = Seconds % 60;
-
-                MilliSeconds = (int) (UpdateTime % 1000);
-
-                ListElementsArrayList.add("Lap "+ LapNumber + "  " + Minutes + ":"
-                        + String.format("%02d", Seconds) + ":"
-                        + String.format("%03d", MilliSeconds));
-
-                TimeLaps = UpdateTime + TimeLaps;
-                LapNumber += 1;
-
-                adapter.notifyDataSetChanged();
-
-            }
-        });
-
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
-    public Runnable runnable = new Runnable() {
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new OneFragment(), "ONE");
+        adapter.addFragment(new TwoFragment(), "TWO");
+        viewPager.setAdapter(adapter);
+    }
 
-        public void run() {
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
 
-            MillisecondTime = SystemClock.uptimeMillis() - StartTime;
-
-            UpdateTime = TimeBuff + MillisecondTime;
-
-            Seconds = (int) (UpdateTime / 1000);
-
-            Minutes = Seconds / 60;
-
-            Seconds = Seconds % 60;
-
-            MilliSeconds = (int) (UpdateTime % 1000);
-
-            textView.setText("" + Minutes + ":"
-                    + String.format("%02d", Seconds) + ":"
-                    + String.format("%03d", MilliSeconds));
-
-            handler.postDelayed(this, 0);
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
         }
 
-    };
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
 }
---
+++ app/src/main/java/com/example/triisaandroid/OneFragment.java
@@ -0,0 +1,169 @@
+package com.example.triisaandroid;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.os.Handler;
+
+import java.sql.Time;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+import android.widget.ArrayAdapter;
+import android.os.SystemClock;
+
+public class OneFragment extends Fragment{
+
+    TextView textView ;
+    ListView listView;
+    Button start, pause, reset, lap ;
+    Handler handler;
+    ArrayAdapter<String> adapter ;
+    List<String> ListElementsArrayList ;
+    String[] ListElements = new String[] {  };
+    long MillisecondTime, StartTime, TimeBuff, TimeLaps, UpdateTime = 0L ;
+    int Seconds, Minutes, MilliSeconds ;
+    int LapNumber = 1;
+
+    public OneFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+        textView = (TextView) view.findViewById(R.id.textView);
+        start = (Button) view.findViewById(R.id.button);
+        pause = (Button) view.findViewById(R.id.button2);
+        reset = (Button) view.findViewById(R.id.button3);
+        lap = (Button) view.findViewById(R.id.button4) ;
+        listView = (ListView) view.findViewById(R.id.listview1);
+
+        handler = new Handler() ;
+
+        ListElementsArrayList = new ArrayList<String>(Arrays.asList(ListElements));
+
+        adapter = new ArrayAdapter<String>(view.getContext(),
+                android.R.layout.simple_list_item_1,
+                ListElementsArrayList
+        );
+
+        listView.setAdapter(adapter);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                StartTime = SystemClock.uptimeMillis();
+                handler.postDelayed(runnable, 0);
+
+                reset.setEnabled(false);
+                pause.setEnabled(true);
+                start.setEnabled(false);
+            }
+        });
+
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TimeBuff += MillisecondTime;
+
+                handler.removeCallbacks(runnable);
+
+                reset.setEnabled(true);
+                pause.setEnabled(false);
+                start.setEnabled(true);
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MillisecondTime = 0L ;
+                StartTime = 0L ;
+                TimeBuff = 0L ;
+                UpdateTime = 0L ;
+                Seconds = 0 ;
+                Minutes = 0 ;
+                MilliSeconds = 0 ;
+                TimeLaps = 0L ;
+                LapNumber = 1;
+
+                textView.setText("00:00:00");
+
+                ListElementsArrayList.clear();
+
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+        lap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                UpdateTime = UpdateTime - TimeLaps;
+
+                Seconds = (int) (UpdateTime / 1000);
+
+                Minutes = Seconds / 60;
+
+                Seconds = Seconds % 60;
+
+                MilliSeconds = (int) (UpdateTime % 1000);
+
+                ListElementsArrayList.add("Lap " + LapNumber + "  " + Minutes + ":"
+                        + String.format("%02d", Seconds) + ":"
+                        + String.format("%03d", MilliSeconds));
+
+                UpdateTime = UpdateTime + TimeLaps;
+                TimeLaps = UpdateTime;
+
+                LapNumber += 1;
+
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+
+        return view;
+    }
+
+    public Runnable runnable = new Runnable() {
+
+        public void run() {
+
+            MillisecondTime = SystemClock.uptimeMillis() - StartTime;
+
+            UpdateTime = TimeBuff + MillisecondTime;
+
+            Seconds = (int) (UpdateTime / 1000);
+
+            Minutes = Seconds / 60;
+
+            Seconds = Seconds % 60;
+
+            MilliSeconds = (int) (UpdateTime % 1000);
+
+            textView.setText("" + Minutes + ":"
+                    + String.format("%02d", Seconds) + ":"
+                    + String.format("%03d", MilliSeconds));
+
+            handler.postDelayed(this, 0);
+        }
+    };
+}
